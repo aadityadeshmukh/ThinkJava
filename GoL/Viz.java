a94304a5f96faa3bab3ae301bdf73b993be06742
Aaditya Deshmukh <aadityadeshmukh@gmail.com>
2022-01-30 10:05:39 +0000

    setting up the Viz class with 2d Cell grid

--- GoL/Viz.java
+++ GoL/Viz.java
@@ -3,25 +3,40 @@ import javax.swing.JFrame;
 import java.awt.Color;
 import java.awt.Graphics;
 public class Viz extends Canvas{
+	public Cell[][] grid;
+	public Viz(int rows, int col, int size){
+		grid = new Cell[rows][col];
+
+		for(int i = 0; i < rows; i++){
+			int x = i * size;
+			for(int j = 0; j < col; j++){
+				int y = j * size;
+
+				grid[i][j] = new Cell(x,y,size);
+			}
+		}
+		setSize(rows*size,col*size);
+	}
+	
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Game of Life");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Viz d = new Viz();
-		d.setSize(400,400);
-		d.setBackground(Color.WHITE);
+		Viz d = new Viz(4,3,20);
+		//d.setBackground(Color.WHITE);
 		frame.add(d);
 		frame.pack();
 		frame.setVisible(true);
 	}
 
-	public void paint(Graphics g){
-		Cell c = new Cell(200,200,10);
-		while(true){
-			if(c.isOff())
-				c.turnOn();
-			else
-				c.turnOff();
-			c.draw(g);
+	public void draw(Graphics g){
+		for(Cell[] row : grid){
+			for(Cell c : row){
+				c.draw(g);
+			}
 		}
+	}
+
+	public void paint(Graphics g){
+		draw(g);
 	}
 }
